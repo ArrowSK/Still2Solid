d5f4f7dd14a3dae4d75d4de321ce8890e9776fbd
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2026-08-28 14:10:50 +0200

    chore: remove redundant background integration wrapper

--- apps/desktop/ui/src/lib/AppBackgroundIntegration.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import BackgroundAdvisor from './BackgroundAdvisor.svelte';
-  export let imageUrl = '';
-  export let enabled = true;
-  export let disabled = false;
-</script>
-
-<BackgroundAdvisor {imageUrl} bind:enabled {disabled} />
